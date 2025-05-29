16f83ca4ce4a489f8fbef7e273ff8c7ad33665f8
wangyanran <840186209@qq.com>
2025-05-29 15:03:08 +0800

    单价小数处理

--- controller/Package.go
+++ controller/Package.go
@@ -289,7 +289,7 @@ func GetFlowDayPackageList(c *gin.Context) {
 			//fmt.Println("price:", price)
 			if unitPrice > 0 {
 				//unit = math.Ceil(unitPrice)
-				oStr := fmt.Sprintf("%.1f", math.Round(unitPrice*10)/10)
+				oStr := fmt.Sprintf("%.0f", math.Floor(unitPrice)) //20250529 舍去小数
 				unit = util.StoF(oStr)
 			}
 
@@ -428,7 +428,7 @@ func GetFlowDayPackageList(c *gin.Context) {
 			//fmt.Println("price:", price)
 			if unitPrice > 0 {
 				//unit = math.Ceil(unitPrice)
-				oStr := fmt.Sprintf("%.0f", math.Round(unitPrice*10)/10)
+				oStr := fmt.Sprintf("%.1f", math.Round(unitPrice*10)/10)
 				unit = util.StoF(oStr)
 			}
 
