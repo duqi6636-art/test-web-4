c2e62dc23b2dbe2643da6133297199fa7974265d
liyaning <liyaning@lingjiangkeji.com>
2025-06-12 14:48:32 +0800

    fix

--- controller/Static.go
+++ controller/Static.go
@@ -856,9 +856,7 @@ func BatchBeforeRecharge(c *gin.Context) {
 			key := fmt.Sprintf("%v_%v", ip, v.ExpireDay)
 			resInfoMap[key] = resInfo
 			if val, ok := balanceMap[resInfo.PakName]; ok {
-				if _, ok := val[ipLog.Country]; !ok {
-					val[ipLog.Country] = v.Balance
-				}
+				val[ipLog.Country] = v.Balance
 			} else {
 				balanceMap[resInfo.PakName] = map[string]int{ipLog.Country: v.Balance}
 			}
